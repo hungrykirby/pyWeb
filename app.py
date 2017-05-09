4aa2243b9f4051329e368058a106992632363ecd
Tutanrhamun <katsuki.hanawa.job@gmail.com>
2017-05-09 20:01:51 +0900

    aaa

--- app.py
+++ app.py
@@ -1,66 +1,60 @@
-# Flask などの必要なライブラリをインポートする
 from flask import Flask, render_template, request, redirect, url_for
 import numpy as np
 import os
 from flask.ext.sqlalchemy import SQLAlchemy
+import MySQLdb
 
+conn = MySQLdb.connect(
+        user='katsuki',
+        passwd='8Lapis6Luna',
+        host='localhost',
+        db='k_testdb',
+        port=3306
+    )
+c = conn.cursor()
+sql = 'create table names (name varchar(32))'
+#c.execute(sql)
 
-# 自身の名称を app という名前でインスタンス化する
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    def __init__(self, username):
-        self.username = username
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-# メッセージをランダムに表示するメソッド
 def picked_up():
     messages = [
         "こんにちは、あなたの名前を入力してください",
         "やあ！お名前は何ですか？",
         "あなたの名前を教えてね"
     ]
-    # NumPy の random.choice で配列からランダムに取り出し
     return np.random.choice(messages)
 
-# ここからウェブアプリケーション用のルーティングを記述
-# index にアクセスしたときの処理
 @app.route('/')
 def index():
     title = "ようこそ"
     message = picked_up()
-    # index.html をレンダリングする
     return render_template('index.html',
                            message=message, title=title)
 
-# /post にアクセスしたときの処理
 @app.route('/post', methods=['GET', 'POST'])
 def post():
     title = "こんにちは"
     if request.method == 'POST':
-        # リクエストフォームから「名前」を取得して
         name = request.form['name']
-        if not db.session.query(User).filter(User.username == name).count():
-            print("1")
-        #    reg = User(name)
-        #    db.session.add(reg)
-        #    db.session.commit()
-        # index.html をレンダリングする
+        sql = 'insert into names values (%s)'
+        c.execute(sql, (name,))
+        conn.commit()
         return render_template('index.html',
                                name=name, title=title)
     else:
-        # エラーなどでリダイレクトしたい場合はこんな感じで
         return redirect(url_for('index'))
+
+@app.route('/look')
+def look():
+    sql = 'select * from names'
+    c.execute(sql)
+    for row in c.fetchall():
+        print('Name:', row[0])
+    return 'datas'
 
 if __name__ == '__main__':
     #app.debug = True # デバッグモード有効化
     #port = int(os.environ.get('PORT', 5000))
     #app.run(port=port)
-    db.create_all()
     app.run()
